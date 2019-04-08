f1e31ff02eb4f72a4f3118f8da971620044d75e9
Greg Wiley <gwiley@cj.com>
2019-04-08 15:36:08 -0700

    fix version

--- Formula/cjhello.rb
+++ Formula/cjhello.rb
@@ -4,10 +4,10 @@ class Cjhello < Formula
   homepage "https://github.com/aztecrex/hello-cjtool"
 
   if OS.mac?
-      url "https://github.com/aztecrex/hello-cjtool/releases/download/v#{version}/hello-cjtool-v#{version}-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/aztecrex/hello-cjtool/releases/download/v0.1.2/hello-cjtool-v0.1.2-x86_64-apple-darwin.tar.gz"
       sha256 "332f50bf4601d85cde7b4268ceb2e685d132375a7b635183d16d27706b7e2dc8"
   elsif OS.linux?
-      url "https://github.com/aztecrex/hello-cjtool/releases/download/v#{version}/hello-cjtool-v#{version}-x86_64-unknown-linux-musl.tar.gz"
+      url "https://github.com/aztecrex/hello-cjtool/releases/download/v0.1.2/hello-cjtool-v0.1.2-x86_64-unknown-linux-musl.tar.gz"
       sha256 "57a70d74049aa2424885cc219382dd2043a06eb0cede5b6cf9e25728ae14ce75"
   end
 
